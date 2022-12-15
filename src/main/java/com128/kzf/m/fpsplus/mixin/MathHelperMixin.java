e8a9098347690583a2698d5382ea771ba51ce8fb
Howard ZHY <96782166+HowardZHY@users.noreply.github.com>
2022-12-15 21:29:47 +0800

    Ver A

--- src/main/java/com128/kzf/m/fpsplus/mixin/MathHelperMixin.java
+++ src/main/java/com128/kzf/m/fpsplus/mixin/MathHelperMixin.java
@@ -6,49 +6,52 @@ import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(MathHelper.class)
 public class MathHelperMixin {
-    private static final float PI = 3.1415927f;
-    private static final float TWO_PI = PI * 2;
-    private static final float HALF_PI = PI / 2;
-    private static final float ONE_AND_HALF_PI = PI + HALF_PI;
-    private static final double DTWO_PI = Math.PI * 2;
+    private static final float RAD, DEG, SIN_TO_COS;
+    private static final int SIN_BITS, SIN_MASK, SIN_MASK2, SIN_COUNT, SIN_COUNT2;
+    private static final float radFull, radToIndex;
+    private static final float degFull, degToIndex;
+    private static final float[] sinFull, sinHalf;
 
-    private static final int SIN_BITS = 14;
-    private static final int SIN_MASK = ~(-1 << SIN_BITS);
-    private static final int SIN_COUNT = SIN_MASK + 1;
-    private static final float[] SIN_TABLE = new float[SIN_COUNT];
-
-    private static final float radFull = TWO_PI;
-    private static final float degFull = 360;
-    private static final float radToIndex = SIN_COUNT / radFull;
-    private static final float degToIndex = SIN_COUNT / degFull;
-
-    private static final float radiansToDegrees = 180f / PI;
-    private static final float radDeg = radiansToDegrees;
-    private static final float degreesToRadians = PI / 180;
-    private static final float degRad = degreesToRadians;
-    private static final float[] table = new float[SIN_COUNT];
     static {
-        for (int i = 0; i < SIN_COUNT; i++)
-            table[i] = (float) Math.sin((i + 0.5f) / SIN_COUNT * radFull);
-        for (int i = 0; i < 360; i += 90)
-            table[(int) (i * degToIndex) & SIN_MASK] = (float) Math.sin(i * degreesToRadians);
-    }
+        RAD = (float) Math.PI / 180.0f;
+        DEG = 180.0f / (float) Math.PI;
+        SIN_TO_COS = (float) (Math.PI * 0.5f);
 
+        SIN_BITS = 12;
+        SIN_MASK = ~(-1 << SIN_BITS);
+        SIN_MASK2 = SIN_MASK >> 1;
+        SIN_COUNT = SIN_MASK + 1;
+        SIN_COUNT2 = SIN_MASK2 + 1;
+
+        radFull = (float) (Math.PI * 2.0);
+        degFull = (float) (360.0);
+        radToIndex = SIN_COUNT / radFull;
+        degToIndex = SIN_COUNT / degFull;
+
+        sinFull = new float[SIN_COUNT];
+        for (int i = 0; i < SIN_COUNT; i++) {
+            sinFull[i] = (float) Math.sin((i + Math.min(1, i % (SIN_COUNT / 4)) * 0.5) / SIN_COUNT * radFull);
+        }
+
+        sinHalf = new float[SIN_COUNT2];
+        for (int i = 0; i < SIN_COUNT2; i++) {
+            sinHalf[i] = (float) Math.sin((i + Math.min(1, i % (SIN_COUNT / 4)) * 0.5) / SIN_COUNT * radFull);
+        }
+    }
     /**
      * @author abandenz
      * @reason HowardZHY
      */
     @Overwrite
-    public static final float sin(float radians) {
-        return table[(int) (radians * radToIndex) & SIN_MASK];
+    public static final float sin(float rad) {
+        return sinFull[(int) (rad * radToIndex) & SIN_MASK];
     }
-
     /**
      * @author abandenz
      * @reason HowardZHY
      */
     @Overwrite
-    public static final float cos(float radians) {
-        return table[(int) ((radians + PI / 2) * radToIndex) & SIN_MASK];
+    public static final float cos(float rad) {
+        return sin(rad + SIN_TO_COS);
     }
 }
